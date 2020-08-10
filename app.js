b2f1c1034cbf3fdff01ac571050dc3a5980197e9
Parth Padhiar <parthpadhiar99@gmail.com>
2020-08-10 20:51:56 +0530

    update query complted

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-
-mongoose.connect("mongodb://localhost:27017/crudDATA")
+mongoose.connect("mongodb://localhost:27017/crudDATA");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
-
 
 const dataSchema = {
     email: String,
@@ -20,17 +20,16 @@ const dataSchema = {
     lastName: String,
     age: Number,
     gender: String,
-    status: String
-}
+    status: String,
+};
 
-const Data = mongoose.model("CrudDATA", dataSchema)
+const Data = mongoose.model("CrudDATA", dataSchema);
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
-app.get('/',(req, res)=>{
-   res.render("home")
-})
-
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     const fname = req.body.fname;
@@ -46,30 +45,48 @@ app.post('/', async (req, res) => {
         lastName: lname,
         age: age,
         gender: gender,
-        status: status
-    })
-    await data.save()
-    res.redirect('/')
-})
+        status: status,
+    });
+    await data.save();
+    res.redirect("/");
+});
 
-
-app.get('/show', async (req, res) => {
+app.get("/show", async (req, res) => {
     await Data.find((err, data) => {
         console.log(data);
         res.render("show", {
-            data: data
-        })
+            data: data,
+        });
+    });
+});
+
+
+app.get("/update", (req, res) => {
+    res.render("update");
+});
+
+app.post("/update", (req, res) => {
+    Data.findOneAndUpdate({ firstName: req.body.updatefname}, { 
+      "$set" :{ "email": req.body.email,
+        "password": req.body.password,
+        "firstName": req.body.fname,
+        "lastName": req.body.lname,
+        "gender": req.body.gender,
+        "age": req.body.age,
+        "status": req.body.status}
+    }, (err) => {
+        console.log('====================================');
+        console.log(Data);
+        console.log('====================================');
+        if (err) {
+            console.log('-----------error-------------', err);
+        } else {
+            console.log("-----------------Updated----------------");
+        }
     })
-})
+    res.redirect("/update");
+});
 
-app.get('/update', (req, res) => {
-    res.render("update")
-})
-
-app.post('/update', (req, res) => {
-    Data.updateOne({})
-})
-
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log("App is running on port 3000");
-})
+});
